f7480fc1857e0199eee40652022786c31cc6cc3a
Nathan <Nathan.Baines64@gmail.com>
2019-01-26 04:42:11 -0500

    Cars & car movement -NB -HS

---
+++ Don't Call Me Late for Dinner/Assets/Scripts/OverworldScripts/CarMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		carUpdate();
+	}
+	
+	void carUpdate()
+	{
+		if(this.gameObject.tag == "VerticalCar")
+			transform.position += (new Vector3 (0, 1f, 0) * speed);
+		if(this.gameObject.tag == "HorizontalCar")
+			transform.position += (new Vector3 (1f, 0, 0) * speed);
+	}
+}
