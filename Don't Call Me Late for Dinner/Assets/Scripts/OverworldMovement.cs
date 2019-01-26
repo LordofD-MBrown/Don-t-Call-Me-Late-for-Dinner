feb8d697b8994a37bc83293931d03f5e62d4810a
LordofD-MBrown <44680178+LordofD-MBrown@users.noreply.github.com>
2019-01-26 02:24:52 -0500

    Revert "deleting file"
    This reverts commit 7e2598b09d7d06f2ca20902fef756f373af99a7f.

---
+++ Don't Call Me Late for Dinner/Assets/Scripts/OverworldMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldMovement : MonoBehaviour
+{
+    public float speed = 20f;
+    public Vector2 move = Vector2.zero;
+	public Rigidbody2D rb2D;
+    void Start()
+    {
+		
+    }
+
+
+    void Update()
+    {
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("holding down 'W'");
+            Vector2 moveInput = new Vector2(0f, 1f);
+            move = moveInput.normalized * speed;
+			rb2D.velocity = move;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("holding down 'S'");
+            Vector2 moveInput = new Vector2(0f, -1f);
+            move = moveInput.normalized * speed;
+            rb2D.velocity = move;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("holding down 'A'");
+            Vector2 moveInput = new Vector2(-1f, 0f);
+            move = moveInput.normalized * speed;
+            rb2D.velocity = move;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("holding down 'D'");
+            Vector2 moveInput = new Vector2(1f, 0f);
+            move = moveInput.normalized * speed;
+            rb2D.velocity = move;
+        }
+    }
+}
