ff4bc5098b0fa89bb56cdf560c36e92eb3f3b160
LordofD-MBrown <44680178+LordofD-MBrown@users.noreply.github.com>
2019-01-26 02:21:35 -0500

    Revert "- merge test"
    This reverts commit f9321ab7843a5f8976eacdb0dac460a04077a437, reversing
    changes made to 67697dd52a5e2973db6ac8337ffc4be463c59185.

--- Don't Call Me Late for Dinner/Assets/Scripts/OverworldMovement.cs
+++ Don't Call Me Late for Dinner/Assets/Scripts/OverworldMovement.cs
@@ -7,6 +7,11 @@ public class OverworldMovement : MonoBehaviour
     public float speed = 20f;
     public Vector2 move = Vector2.zero;
 	public Rigidbody2D rb2D;
+    void Start()
+    {
+		
+    }
+
 
     void Update()
     {
@@ -15,42 +20,33 @@ public class OverworldMovement : MonoBehaviour
 
     void movePlayer()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            move = new Vector2(0f, speed);
-			rb2D.velocity += move;
+            Debug.Log("holding down 'W'");
+            Vector2 moveInput = new Vector2(0f, 1f);
+            move = moveInput.normalized * speed;
+			rb2D.velocity = move;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            move = new Vector2(0f, -speed);
-            rb2D.velocity += move;
+            Debug.Log("holding down 'S'");
+            Vector2 moveInput = new Vector2(0f, -1f);
+            move = moveInput.normalized * speed;
+            rb2D.velocity = move;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
-<<<<<<< HEAD
             Debug.Log("holding down 'A'");
             Vector2 moveInput = new Vector2(-1f, 0f);
             move = moveInput.normalized * speed;
             rb2D.velocity = move;
-=======
-            move= new Vector2(-speed, 0f);
-            rb2D.velocity += move;
->>>>>>> NB-Branch
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            move = new Vector2(speed, 0f);
-            rb2D.velocity += move;
+            Debug.Log("holding down 'D'");
+            Vector2 moveInput = new Vector2(1f, 0f);
+            move = moveInput.normalized * speed;
+            rb2D.velocity = move;
         }
-		if(rb2D.velocity != Vector2.zero)
-		{
-			if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-				rb2D.velocity += new Vector2(0f, -rb2D.velocity.y);
-			if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-				rb2D.velocity += new Vector2(-rb2D.velocity.x, 0f);
-			
-		}
-		
-		
     }
 }
