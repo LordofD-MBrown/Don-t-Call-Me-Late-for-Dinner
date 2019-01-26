3810df384f509544bd91db8c05402641f69bdb05
Nathan <Nathan.Baines64@gmail.com>
2019-01-26 05:16:20 -0500

    Added in car movement -NB -HS -DW

--- Don't Call Me Late for Dinner/Assets/Scripts/OverworldScripts/CarMovement.cs
+++ Don't Call Me Late for Dinner/Assets/Scripts/OverworldScripts/CarMovement.cs
@@ -24,4 +24,17 @@ public class CarMovement : MonoBehaviour {
 		if(this.gameObject.tag == "HorizontalCar")
 			transform.position += (new Vector3 (1f, 0, 0) * speed);
 	}
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "VerticalCarReset")
+		{
+			Debug.Log(col.gameObject.tag);
+			transform.position = new Vector3(14.54f, -33.86f, 0f);
+		}
+		if(col.gameObject.tag == "HorizontalCarReset")
+		{
+			transform.position = new Vector3(-16.34f, -6.46f, 0f);
+		}
+	}
+	
 }
