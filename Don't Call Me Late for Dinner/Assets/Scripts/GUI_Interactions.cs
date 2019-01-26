fa86a040ab6257ef38ec8c530f80e0e1d43c5d82
zmoore00 <zmoore@monroeccc.edu>
2019-01-25 21:58:37 -0500

    Added basic movement -NB

---
+++ Don't Call Me Late for Dinner/Assets/Scripts/GUI_Interactions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUI_Interactions : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Avery - Test Scene");
+    }
+}
---
+++ Don't Call Me Late for Dinner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20f;
+    public float jumpForce = 10f;
+    public float gravity = 30f;
+    public Vector3 move = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("holding down 'W'");
+            Vector3 moveInput = new Vector3(0f, 1f, 0f);
+            move = moveInput.normalized * speed;
+            transform.position += move * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("holding down 'S'");
+            Vector3 moveInput = new Vector3(0f, -1f, 0f);
+            move = moveInput.normalized * speed;
+            transform.position += move * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("holding down 'A'");
+            Vector3 moveInput = new Vector3(-1f, 0f, 0f);
+            move = moveInput.normalized * speed;
+            transform.position += move * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("holding down 'D'");
+            Vector3 moveInput = new Vector3(1f, 0f, 0f);
+            move = moveInput.normalized * speed;
+            transform.position += move * Time.deltaTime;
+        }
+    }
+}
